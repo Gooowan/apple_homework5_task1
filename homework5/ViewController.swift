1df8f955d634826ea02cb77c24b8fa92c92f62de
BUNJEE <vsolomatin@kse.org.ua>
2024-11-27 13:00:11 +0200

    Initial Commit

---
+++ homework5/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  homework5
+//
+//  Created by Ivan Solomatin on 27.11.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ homework5Tests/homework5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  homework5Tests.swift
+//  homework5Tests
+//
+//  Created by Ivan Solomatin on 27.11.2024.
+//
+
+import Testing
+@testable import homework5
+
+struct homework5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
